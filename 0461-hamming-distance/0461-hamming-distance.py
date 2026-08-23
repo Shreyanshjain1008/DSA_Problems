411192629b990be88a468c11c4e7896368f915fe
Shreyansh Jain <jainshreyansh1411@gmail.com>
2026-08-23 11:10:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.54%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        n = x ^ y
+        count = 0
+
+        while n:
+            n = n & (n - 1)
+            count += 1
+
+        return count
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        
